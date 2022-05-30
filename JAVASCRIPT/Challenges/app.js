410008274343cb299b58d412ae3551ddb0726d6c
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-05-30 19:18:06 +0100

    finished fix code 2

--- JAVASCRIPT/Challenges/app.js
+++ JAVASCRIPT/Challenges/app.js
@@ -155,3 +155,60 @@ const howOld = (age, year) => {
     return `You were ${newAge} in the year ${year}`;
   }
 };
+
+// !Fix broken code 2
+
+// const whatRelation = percentSharedDNA => {
+//     if (percentSharedDNA === 100) {
+//         return 'You are likely identical twins.'
+//     }
+//     if (percentSharedDNA > 34) {
+//         return 'You are likely parent and child or full siblings.'
+//     }
+//     if (percentSharedDNA < 13) {
+//         return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+//     }
+//     if (percentSharedDNA > 5) {
+//         return 'You are likely 1st cousins.'
+//     }
+//     if (percentSharedDNA > 2) {
+//         return 'You are likely 2nd cousins.'
+//     }
+//     if (percentSharedDNA > 0) {
+//         return 'You are likely 3rd cousins'
+//     }
+//     return 'You are likely not related.'
+// }
+
+// console.log(whatRelation(34))
+// // Should print 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+
+// console.log(whatRelation(3))
+// // Should print 'You are likely 2nd cousins.'
+const whatRelation = (percentSharedDNA) => {
+  if (percentSharedDNA === 100) {
+    return 'You are likely identical twins.';
+  }
+  if (percentSharedDNA >= 35 && percentSharedDNA <= 99) {
+    return 'You are likely parent and child or full siblings.';
+  }
+  if (percentSharedDNA >= 14 && percentSharedDNA <= 34) {
+    return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.';
+  }
+  if (percentSharedDNA >= 6 && percentSharedDNA <= 13) {
+    return 'You are likely 1st cousins.';
+  }
+  if (percentSharedDNA >= 3 && percentSharedDNA <= 5) {
+    return 'You are likely 2nd cousins.';
+  }
+  if (percentSharedDNA >= 1 && percentSharedDNA <= 2) {
+    return 'You are likely 3rd cousins';
+  }
+  return 'You are likely not related.';
+};
+
+console.log(whatRelation(34));
+// Should print 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+
+console.log(whatRelation(3));
+// Should print 'You are likely 2nd cousins.'
