42782bc0ac18775fedd838798ff0d99e4fac2bff
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-05-30 18:45:31 +0100

    finished weight on planets

--- JAVASCRIPT/Challenges/app.js
+++ JAVASCRIPT/Challenges/app.js
@@ -71,3 +71,21 @@ const rollTheDice = () => {
 };
 
 console.log(rollTheDice());
+
+const calculateWeight = (earthWeight, planet) => {
+  if (planet === 'Mercury') {
+    return `Your weight on ${planet} will be ` + earthWeight * 0.378;
+  } else if (planet === 'Venus') {
+    return `Your weight on ${planet} will be ` + earthWeight * 0.907;
+  } else if (planet === 'Mars') {
+    return `Your weight on ${planet} will be ` + earthWeight * 0.377;
+  } else if (planet === 'Jupiter') {
+    return `Your weight on ${planet} will be ` + earthWeight * 2.36;
+  } else if (planet === 'Saturn') {
+    return `Your weight on ${planet} will be ` + earthWeight * 0.916;
+  } else {
+    return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
+  }
+};
+
+console.log(calculateWeight(100, 'Jupiter'));
