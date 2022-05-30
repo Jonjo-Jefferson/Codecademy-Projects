69e2e2fe508e4c8637d8904fb62f617cab35f90b
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-05-30 19:17:49 +0100

    Finished how old

--- JAVASCRIPT/Challenges/app.js
+++ JAVASCRIPT/Challenges/app.js
@@ -134,3 +134,24 @@ const sillySentence = (adj, verb, noun) => {
   return `I am so ${adj} because I ${verb} coding! Time to write some more awesome ${noun}!`;
 };
 console.log(sillySentence('excited', 'love', 'functions'));
+
+// ! How Old
+
+/* 
+  Our solution is written as a function expression and uses string interpolation, but it would be equally acceptable to use a function declaration and/or string concatenation
+  */
+
+const howOld = (age, year) => {
+  let dateToday = new Date();
+  let thisYear = dateToday.getFullYear();
+
+  const yearDifference = year - thisYear;
+  const newAge = age + yearDifference;
+  if (newAge > age) {
+    return `You will be ${newAge} in the year ${year}`;
+  } else if (newAge < 0) {
+    return `The year ${year} was ${-newAge} years before you were born`;
+  } else {
+    return `You were ${newAge} in the year ${year}`;
+  }
+};
